db09bd60a0d27f5216990cfea3bf0c36f94c5715
xiaoping.zhong.ext <xiaoping.zhong.ext@siemens.com>
2020-08-05 15:14:53 +0800

    :art:

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 # 自动部署脚本  
+git add -A
+git commit -m $1
+git push
 # 构建
 npm run build
 # 导航到构建输出目录
 cd docs/.vuepress/dist
-echo 'blog.fucos.top' >CNAME
+#echo 'blog.fucos.top' >CNAME
 git init
 git add -A
 git commit -m $1
